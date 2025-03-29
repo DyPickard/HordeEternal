8d19238b1037871f868a088ac4c74a20f8bb1e1c
sunidhi04 <86991821+sunidhi04@users.noreply.github.com>
2025-03-29 01:56:47 -0700

    Function Complete
    Bullet targets closest enemy and flies toward them, the "explosion" is a placeholder for an animation, projectile doesnt have any cool sprite yet.
    But, basic functionalities are working and should be easy to integrate with the code for damage on enemies.

--- Assets/Scripts/Attacks/Bolt_Behavior.cs
+++ Assets/Scripts/Attacks/Bolt_Behavior.cs
@@ -3,17 +3,36 @@ using UnityEngine;
 public class Bolt_Behavior : MonoBehaviour
 {
     public float projectilespd;
+    public GameObject impactef;
 
     private Rigidbody2D rigidbody;
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.linearVelocity = transform.right * projectilespd;
-    }
+        float distance = Mathf.Infinity;
+        Enemy closest = null;
+        Enemy[] allEnemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+        
+        foreach (Enemy currentE in allEnemies)
+        {
+            float distanceIteration = (currentE.transform.position - transform.position).sqrMagnitude;
+            if (distanceIteration < distance)
+            {
+                distance = distanceIteration;
+                closest = currentE;
+            }
+        }
 
-    // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
+        Vector3 aim = closest.transform.position - transform.position;
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.linearVelocity = new Vector2(aim.x, aim.y).normalized * projectilespd;
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(gameObject);
+        if (other.CompareTag("Player") == false)
+        {
+            Instantiate(impactef, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
